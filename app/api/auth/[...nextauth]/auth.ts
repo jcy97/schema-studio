b126fff019eba7f5723c614e821885a14c043022
jcy97 <sook3427@naver.com>
2025-05-19 12:24:04 +0900

    [fix] 배포용 라우터 오류 수정

---
+++ app/api/auth/[...nextauth]/auth.ts
@@ -0,0 +1,35 @@
+import GoogleProvider from "next-auth/providers/google";
+
+export const authOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      authorization: {
+        params: {
+          scope:
+            "https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/drive.file",
+          prompt: "consent",
+          access_type: "offline",
+          response_type: "code",
+        },
+      },
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, account }: any) {
+      if (account) {
+        token.accessToken = account.access_token;
+        token.refreshToken = account.refresh_token;
+        token.expiresAt = account.expires_at;
+      }
+      return token;
+    },
+    async session({ session, token }: any) {
+      session.accessToken = token.accessToken;
+      session.expiresAt = token.expiresAt;
+      return session;
+    },
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+};
